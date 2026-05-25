ff2f784407b626a51588b5f7c843c9b39bcf0b75
gnsgj <camellus@naver.com>
2026-05-25 17:58:18 +0900

    Update send.js

--- api/send.js
+++ api/send.js
@@ -88,23 +88,25 @@ export default async function handler(req, res) {
 
         <br><br><br>
 
-    <div style="
-      color:#666;
-      font-size:13px;
-      line-height:1.8;
-    ">
+  <div style="
+  color:#666;
+  font-size:13px;
+  line-height:1.5;
+">
 
-      김만식
-      센터장
-      CS3팀 광주센터 | 지앤서비스
-      ---------------------------------
-      T: 042-932-2206
-      M: 010-2382-0313
-      E: kimms@gnsvce.com
-      W: www.gnsvce.com
-      광주광역시 광산구 산월동 854-6번지
+  김만식<br>
+  센터장<br>
+  CS3팀 광주센터 | 지앤서비스<br>
+  ---------------------------------<br>
 
-    </div>
+  T: 042-932-2206<br>
+  M: 010-2382-0313<br>
+  E: kimms@gnsvce.com<br>
+  W: www.gnsvce.com<br>
+
+  광주광역시 광산구 산월동 854-6번지
+
+</div>
 
   </div>
 
